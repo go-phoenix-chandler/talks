0f735ad3f0bbc4b08a3d9cec4466fe21bb3e5786
bdowns <brian.downs@gmail.com>
2015-01-01 10:14:12 -0700

    initial commit for go14it lightning talk.  Still WIP

---
+++ go14it/src/examples.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type Languages int
+
+//go:generate stringer -type=Languages
+const (
+	Irish Languages = iota
+	Gaelic
+	Welsh
+	Gaeilge = Irish
+)
+
+func NewForExample() {
+	stuff := []string{"Happy", "New", "Year"}
+	for range stuff {
+		fmt.Println("doing stuff...")
+	}
+}
